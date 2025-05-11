bee0f8ace181e7313d550732d641c61f834bdd69
VisionAryavart <automobileszoneofficial@gmail.com>
2025-05-11 23:41:11 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
 
 export default nextConfig;
